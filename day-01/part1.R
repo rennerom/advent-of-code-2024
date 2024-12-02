9aad49ab3b18b164067457d6fd7f1654754af639
Max Renner <rennerom@gmail.com>
2024-12-01 21:26:29 -0600

    day-1 cleanup

--- day-01/part1.R
+++ day-01/part1.R
@@ -1,16 +1,17 @@
-library(dplyr)
-library(readr)
-library(glue)
-
 path <- "day-01/input.txt"
 
-input <- read_fwf(path, fwf_empty(path, col_name = c("one", "two")))
+fetch_column <- function(path, col_number) {
+  data <- readr::read_lines(path) |>
+    stringr::str_split("\\s+") |>
+    purrr::map_chr(col_number)
 
-one <- input$one |> sort()
-two <- input$two |> sort()
+  return(data)
+}
+one <- fetch_column(path, 1) |> as.numeric() |> sort()
+two <- fetch_column(path, 2) |> as.numeric() |> sort()
 
 three <- abs(one - two)
 
 result <- sum(three)
 
-glue("answer: {result}")
+glue::glue("answer: {result}")
--- day-01/part2.R
+++ day-01/part2.R
@@ -1,13 +1,15 @@
-library(dplyr)
-library(readr)
-
 path <- "day-01/input.txt"
 
-input <- read_fwf(path, fwf_empty(path, col_name = c("one", "two")))
+fetch_column <- function(path, col_number) {
+  data <- readr::read_lines(path) |>
+    stringr::str_split("\\s+") |>
+    purrr::map_chr(col_number)
 
-one <- input$one
-two <- input$two
+  return(data)
+}
 
+one <- fetch_column(path, 1) |> as.numeric()
+two <- fetch_column(path, 2) |> as.numeric()
 result_vector <- numeric()
 
 for (i in seq_along(one)) {
@@ -18,4 +20,4 @@ for (i in seq_along(one)) {
 
 result <- sum(result_vector)
 
-glue("answer: {result}")
+glue::glue("answer: {result}")
